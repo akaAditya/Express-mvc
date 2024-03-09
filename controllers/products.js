135af0a841df1d710a3f2f010a69169ec34a0f3e
akaaditya <mymail.adityapatel@gmail.com>
2024-03-10 02:36:20 +0530

    second commit use of Models

--- controllers/products.js
+++ controllers/products.js
@@ -1,27 +1,30 @@
-const products = []
+const Products = require("../models/products");
 
 exports.getAddProducts = (req, res, next) => {
-  res.render('add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
     formsCSS: true,
     productCSS: true,
-    activeAddProduct: true
+    activeAddProduct: true,
   });
-}
+};
 
 exports.postAddProducts = (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect('/');
-}
+  const products = new Products(req.body.title);
+  products.save();
+  res.redirect("/");
+};
 
 exports.getProducts = (req, res, next) => {
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true
-  });
-}
+  Products.FetchAll((products)=>{
+    res.render("shop", {
+      prods: products,
+      pageTitle: "Shop",
+      path: "/",
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true,
+    })
+});
+};
