4934572b0b1c01a78542ae9a7a6b08b0973cb45b
akaaditya <mymail.adityapatel@gmail.com>
2024-03-11 02:59:14 +0530

    third commit dynamic routes 1

--- controllers/error404.js
+++
@@ -1,3 +0,0 @@
-exports.getErrorPage = (req, res, next) => {
-  res.status(404).send("404", { pageTitle: "Page Not Found" });
-};
--- routes/admin.js
+++ routes/admin.js
@@ -2,18 +2,17 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
-
-const productsControllers = require('../controllers/products');
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-const products = [];
-
 // /admin/add-product => GET
-router.get('/add-product', productsControllers.getAddProducts);
+router.get('/add-product', adminController.getAddProduct);
+
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', productsControllers.postAddProducts);
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
